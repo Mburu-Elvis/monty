988187fb5e052d41d398ea40c922a87cdc41aedf
root <sandramabonga303@gmail.com>
2023-08-24 16:58:07 -0700

    swap.c debug

--- swap.c
+++ swap.c
@@ -11,7 +11,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int n1, n2;
 
-	if ((*stack)->prev == NULL || *stack == NULL)
+	if (((*stack)->prev == NULL && (*stack)->next == NULL) || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
